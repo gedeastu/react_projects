7e7584470da0fb8bee86056bc2a8f0381cce320c
Gede Astu Nugraha <gede.astu@gmail.com>
2024-03-19 21:49:30 +0700

    fixing update logic

--- books-react/src/App.jsx
+++ books-react/src/App.jsx
@@ -1,5 +1,6 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import './App.css'
+import axios from 'axios'
 import BookCreate from './components/BookCreate'
 import BookShow from './components/BookShow'
 import BookList from './components/BookList'
@@ -7,28 +8,40 @@ import BookList from './components/BookList'
 function App() {
   const [books,setBooks] = useState([])
 
-  const handleCreateBook = (title) => {
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books')
+    setBooks(response.data)
+  }
+
+  useEffect(()=>{
+    fetchBooks()
+  },[])
+
+  const handleCreateBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books',{
+      title:title,
+    })
     const updatedBooks = [
       ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title: title
-      }
     ]
     setBooks(updatedBooks)
   }
 
-  const handleDeleteBookById = (id) => {
+  const handleDeleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`)
     const updatedBooks = books.filter((book)=>{
       return book.id != id
     })
     setBooks(updatedBooks)
   }
 
-  const handleEditedByIndex = (id, newTitle) => {
+  const handleEditedByIndex = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`,{
+      title : newTitle
+    })
     const updatedBooks = books.map((book)=>{
       if (book.id == id) {
-        return{...book, title:newTitle}
+        return{...book, ...response.data,}
       }
       return book
     })
